f1833069de472366ff4a24ce9cbaca0bd414d64e
jch <jeongchihong@sk.com>
2025-04-10 10:59:00 +0900

    Initial Commit

---
+++ AppStoreAPI/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AppStoreAPI
+//
+//  Created by 201510089 on 4/10/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ AppStoreAPITests/AppStoreAPITests.swift
@@ -0,0 +1,17 @@
+//
+//  AppStoreAPITests.swift
+//  AppStoreAPITests
+//
+//  Created by 201510089 on 4/10/25.
+//
+
+import Testing
+@testable import AppStoreAPI
+
+struct AppStoreAPITests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
